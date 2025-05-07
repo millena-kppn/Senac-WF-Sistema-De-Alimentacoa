9af536287decb7d514b1e8fa91c1ca65020d0759
Millena Kappaun <kappaunmillena@gmail.com>
2025-05-06 21:50:55 -0300

    conclusão da validação do usuario

--- SenacAlimentacaoSolucao/SenacAlimentacao/FrmLogin.cs
+++ SenacAlimentacaoSolucao/SenacAlimentacao/FrmLogin.cs
@@ -7,14 +7,38 @@ namespace SenacAlimentacao
             InitializeComponent();
         }
 
-        private void btnEntrar_Click(object sender, EventArgs e)
+        private void btnEntrar_Click(object sender, EventArgs e) 
         {
-            //ocultar o formulário FrmLogin
-            this.Hide();
-            //criar uma instância do formulário FrmPrincipal
-            var frmPrincipal = new FrmPrincipal(txtLogin.Text, txtSenha.Text);
-            //exibir o formulário FrmPrincipal
-            frmPrincipal.Show();
+            bool loginValido = ValidarLogin(txtLogin.Text, txtSenha.Text);
+            //se o login for válido ele exibe o formulário principal
+            if (loginValido == true)
+                {
+                    //ocultar o formulário FrmLogin
+                    this.Hide();
+                    //criar uma instância do formulário FrmPrincipal
+                    var frmPrincipal = new FrmPrincipal(txtLogin.Text, txtSenha.Text);
+                    //exibir o formulário FrmPrincipal
+                    frmPrincipal.Show();
+                }
+        }
+        
+
+        private bool ValidarLogin(string nome, string senha)
+        {
+
+            //se nome é igual a "Millena" e senha é igual a "123"
+            if (nome == "Millena" && senha == "123")
+            {
+            //verifica se o nome e a senha estão corretos RETORNA VERDADEIRO
+                return true;
+            }
+            else
+            //exibe uma mensagem de erro
+            {
+                MessageBox.Show("Nome ou senha inválidos!");
+            }
+            //verifica se o nome e a senha estão corretos RETORNA FALSO
+            return false;
         }
 
         private void btnFecharLogin_Click(object sender, EventArgs e)
