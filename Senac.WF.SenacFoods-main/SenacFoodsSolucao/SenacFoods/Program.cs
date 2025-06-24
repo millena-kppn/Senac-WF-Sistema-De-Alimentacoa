dbfa46cd90b1c3aebd2340457083a3ae0c6c5ec9
Millena Kappaun <kappaunmillena@gmail.com>
2025-06-24 19:50:56 -0300

    atualização pastas

--- Senac.WF.SenacFoods-main/SenacFoodsSolucao/SenacFoods/Program.cs
+++ Senac.WF.SenacFoods-main/SenacFoodsSolucao/SenacFoods/Program.cs
@@ -1,4 +1,4 @@
-namespace SenacAlimentacao
+namespace SenacFoods
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace SenacAlimentacao
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new FrmLogin());
+            Application.Run(new Form1());
         }
     }
 }
